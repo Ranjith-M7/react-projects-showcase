861c4d9747ec9f08c4ce16e926cc61e89c5328b2
Ranjith-M7 <ranjith.mathiyazhagan@gmail.com>
2024-08-17 21:26:37 +0530

    updated

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,9 +1,57 @@
-import React from 'react'
+import React, { useState } from "react";
 
+const generatedId = () => {
+  return Math.floor(Math.random() * 1000);
+};
 function TodoApp() {
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleClick = () => {
+    if (inputValue.trim() === "") return;
+
+    setTodos(
+      todos.concat({
+        name: inputValue,
+        id: generatedId(),
+      })
+    );
+    setInputValue("");
+  };
+
+  const deleteTask = (id) => {
+    setTodos(
+      todos.filter((t) => {
+        return t.id != id;
+      })
+    );
+  };
+
   return (
-    <div>TodoApp</div>
-  )
+    <>
+      <div className="todo">
+        <div className="todo-container">
+          <div className="div">
+            <input
+              type="text"
+              placeholder="Enter task"
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+            />
+            <button onClick={handleClick}>+</button>
+          </div>
+          <ul>
+            {todos.map(({ name, id }, index) => (
+              <li key={index}>
+                <p>{name}</p>
+                <p onClick={() => deleteTask(id)}>X</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default TodoApp
+export default TodoApp;
