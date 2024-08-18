8c3af92730c88d3abab7d02907cdb1b2e8a26026
Ranjith-M7 <ranjith.mathiyazhagan@gmail.com>
2024-08-18 23:01:43 +0530

    added testimonial

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function Testimonial() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const testimonials = [
+    {
+      quote: "This is the best product I've ever used!",
+      author: "Leo das",
+    },
+    {
+      quote:
+        "The blue color and the design makes this product even more beautiful",
+      author: "Granny Tsunade",
+    },
+    {
+      quote: "I have been using the product more than a year!",
+      author: "Thamizhini Akilan",
+    },
+  ];
+
+  const handlePrev = () => {
+    const a = (currentIndex + testimonials.length - 1) % testimonials.length;
+    setCurrentIndex(a);
+    console.log(a);
+  };
+
+  const handleNext = () => {
+    const a = (currentIndex + 1) % testimonials.length;
+    setCurrentIndex(a);
+  };
+
   return (
-    <div>Testimonial</div>
-  )
+    <>
+      <div className="testimonials">
+        <div className="t-container">
+          <div className="testimonial">
+            <div className="quote">{testimonials[currentIndex].quote}</div>
+            <div className="author">- {testimonials[currentIndex].author}</div>
+            <div className="btns d-flex justify-content-around align-items-center">
+              <button onClick={() => handlePrev()}>
+                <i className="fa-solid fa-arrow-left"></i>
+              </button>
+              <button onClick={() => handleNext()}>
+                <i className="fa-solid fa-arrow-right"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Testimonial
+export default Testimonial;
