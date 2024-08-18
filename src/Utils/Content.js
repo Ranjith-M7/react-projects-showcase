72d9e5625e7763cb7a018188830028014adfd290
Ranjith-M7 <ranjith.mathiyazhagan@gmail.com>
2024-08-18 15:40:55 +0530

    added accordion

---
+++ src/Utils/Content.js
@@ -0,0 +1,22 @@
+export const accordionData = [
+  {
+    title: "What is the C programming language, and why is it important?",
+    content:
+      "C is a general-purpose programming language developed in the early 1970s. It is known for its efficiency and control over system resources, making it essential for system programming",
+  },
+  {
+    title:
+      "What is Java, and how does it differ from other programming languages?",
+    content: `Java is an object-oriented programming language known for its "write once, run anywhere" capability, meaning compiled Java code can run on any platform that supports Java without modification.`,
+  },
+  {
+    title: "What is React, and what are its key features?",
+    content:
+      "React is a popular JavaScript library for building user interfaces, particularly single-page applications.",
+  },
+  {
+    title: "What is Go (Golang)?",
+    content:
+      "Go, also known as Golang, is a statically typed, compiled programming language designed by Google. ",
+  },
+];
--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useState } from "react";
+import { accordionData } from "../Utils/Content";
 
 function Accordion() {
+  const [activeIndex, setActiveIndex] = useState(false);
+
+  const toggleAccordion = (index) => {
+    setActiveIndex(activeIndex === index ? null : index);
+  };
+
   return (
-    <div>Accordion</div>
-  )
+    <>
+      <div className="accordion d-flex justify-content-center align-items-center">
+        <div className="acc-container">
+          <h1>Accordion</h1>
+          {accordionData.map(({ title, content }, index) => (
+            <div key={index} className="list">
+              <div
+                className="header d-flex justify-content-between align-items-center"
+                onClick={() => toggleAccordion(index)}
+              >
+                <span className="question">{title}</span>
+                <button>
+                  <i
+                    className={`fa-solid ${
+                      activeIndex === index ? "fa-minus" : "fa-plus"
+                    }`}
+                  ></i>
+                </button>
+              </div>
+              {activeIndex === index && (
+                <div className="content">
+                  <p>{content}</p>
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Accordion;
