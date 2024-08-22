cb67a983fcf2649a0a252b4fdfe67aa7722e4a20
Ranjith-M7 <ranjith.mathiyazhagan@gmail.com>
2024-08-22 14:22:57 +0530

    added toggle-search-app

--- src/components/ToggleSearch.js
+++ src/components/ToggleSearch.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function ToggleSearch() {
+  const [showInput, setShowInput] = useState(false);
+  const [bgColor, setBgColor] = useState();
+
+  const handleClick = (e) => {
+    setBgColor("blueviolet");
+    if (e.target.className === "search-container") {
+      setShowInput(false);
+      setBgColor();
+    }
+  };
+
   return (
-    <div>ToggleSearch</div>
-  )
+    <>
+      <div
+        className="search-container"
+        onClick={handleClick}
+        style={{ backgroundColor: bgColor }}
+      >
+        {showInput ? (
+          <input
+            type="text"
+            placeholder="Search..."
+            className="border shadow p-2"
+          />
+        ) : (
+          <i
+            class="fa-solid fa-magnifying-glass"
+            onClick={() => setShowInput(true)}
+          ></i>
+        )}
+      </div>
+    </>
+  );
 }
 
-export default ToggleSearch
+export default ToggleSearch;
